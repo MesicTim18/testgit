64fb9a4545e05e1fffba154b7eb2e3e62335be2e
MesicTim18 <timmmm01982@gmail.com>
2019-01-13 17:22:16 +0100

    Dodana vrstica Hello Local.

--- Hello.cs
+++ Hello.cs
@@ -1,9 +1,18 @@
 // Ime datoteke: Hello.cs
 public class Hello
 {
+<<<<<<< HEAD
    public static void Main()
    {
 	System.Console.WriteLine("Hello, World!");
 	System.Console.WriteLine("Hello, GitHub!");
    }
 }
+=======
+    public static void Main()
+    {
+        System.Console.WriteLine("Hello, World!");
+        System.Console.WriteLine("Hello, Local!");
+    }
+}
+>>>>>>> Dodana vrstica Hello Local.

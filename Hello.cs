822c09d4464077e0791283f391a862eaae2db7dd
MesicTim18 <timmmm01982@gmail.com>
2019-01-13 17:27:33 +0100

    Razrešen konflikt.

--- Hello.cs
+++ Hello.cs
@@ -1,18 +1,10 @@
 // Ime datoteke: Hello.cs
 public class Hello
 {
-<<<<<<< HEAD
-   public static void Main()
-   {
-	System.Console.WriteLine("Hello, World!");
-	System.Console.WriteLine("Hello, GitHub!");
-   }
-}
-=======
     public static void Main()
     {
         System.Console.WriteLine("Hello, World!");
+        System.Console.WriteLine("Hello, GitHub!");
         System.Console.WriteLine("Hello, Local!");
     }
 }
->>>>>>> Dodana vrstica Hello Local.
